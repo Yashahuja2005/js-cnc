2d85705c660f0c9806b372045ef9c5cb312a91b9
Yashahuja2005 <ahujayash756@gmail.com>
2025-10-24 00:07:47 +0530

    dt1

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!")
+const accId = 12345
+let accEmail = "yash@gmail.com"
+var accPass ="125"
+accCity = "Delhi"
+let accState
+
+// accId = 2   // not allowed
+
+console.log(accId);
+
+
+/* Prefer not to use var
+because of issue in block scope and functional scope */
+
+
+console.table([accId, accEmail, accPass, accCity, accState])
