b4d982d529dfc1563c5628dca76fc44f36b545d9
Yashahuja2005 <ahujayash756@gmail.com>
2025-10-24 16:10:02 +0530

    Datatypes summary

---
+++ 01_basics/04_dtSummary.js
@@ -0,0 +1,36 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, undefined, null, Symbol, BigInt
+
+/* const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const bigNumber=345678545466421256n
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId); */
+
+// "===" => strict check
+
+// Reference (Non-Primitive)
+
+// Array, Objects, functions
+
+/* const heros = ["Krrish", "Shaktiman", "Flying-Jatt"]
+let myObj = {
+    name: "Yash", 
+    age: 22
+}
+
+const myFunc = function(){
+    console.log("Hello World");
+    
+} */
+
+
+
+
