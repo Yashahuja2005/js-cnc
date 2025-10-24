1783cbbf63768c2e52df13714f933b171af8847a
Yashahuja2005 <ahujayash756@gmail.com>
2025-10-24 15:24:21 +0530

    conversion of datatype

---
+++ 01_basics/03_convOp.js
@@ -0,0 +1,37 @@
+/* let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+
+let an = null
+console.log(typeof an);
+let valInNumber = Number(an)
+console.log(typeof valInNumber);
+console.log(valInNumber); */
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// "" => false
+// "anyNonEmptyString" => true
+
+let no = 33
+let strNo = String(no)
+console.log(strNo);
+console.log(typeof strNo);
+
+
+
